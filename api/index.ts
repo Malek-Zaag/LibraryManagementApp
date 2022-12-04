82a6ad3e1751595fa521bcdd68d239148c510bea
malek <zaag.malek1@gmail.com>
2022-12-04 22:03:30 +0100

    .

--- api/index.ts
+++ api/index.ts
@@ -22,13 +22,6 @@ app.listen(5050, () => {
   console.log("server listening on port 5050");
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log("test worked for get request");
-  res.send("hello from backend 2");
-  console.log("help me out!!!!");
-});
-app.use(router);
-
 connection
   .sync()
   .then(() => {
@@ -37,3 +30,10 @@ connection
   .catch((err) => {
     console.log(err);
   });
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log("test worked for get request");
+  res.send("hello from backend 2");
+  console.log("help me out!!!!");
+});
+app.use(router);
