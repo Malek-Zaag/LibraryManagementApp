9aa7246507d2d8be0c67d609f3efc336d8371c57
malek <zaag.malek1@gmail.com>
2022-12-13 10:31:05 +0100

    testing things

--- api/config.js
+++ api/config.js
@@ -3,13 +3,14 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.db,
-  process.env.username,
+  process.env.user || "root",
   process.env.password,
   {
     host: process.env.host,
     dialect: process.env.dialect,
   }
 );
+console.log(process.env.db, process.env.user, process.env.password);
 sequelize
   .authenticate()
   .then(() => {
--- api/routes.js
+++ api/routes.js
@@ -5,10 +5,10 @@ const router = Router();
 
 router.get("/api/", controller.simpleGetRequest);
 router.get("/api/db", controller.dbCall);
-router.post("/api/user/signup", controller.signUp);
-router.post("/api/user/login", controller.login);
 router.get("/api/user/", controller.getAllUsers);
 router.get("/api/books", controller.getAllBooks);
+router.post("/api/user/signup", controller.signUp);
+router.post("/api/user/login", controller.login);
 router.post("/api/books/:bookid", controller.addReview);
 router.post("/api/books", controller.addBook);
 
